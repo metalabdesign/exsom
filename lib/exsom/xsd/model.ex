69453df36caebdeefc8a9dc08ba6f23d3d9e1b12
Steven Vandevelde <icid.asset@gmail.com>
2016-08-05 11:54:18 +0200

    Add XSD Model utils docs and specs

--- lib/exsom/xsd/model.ex
+++ lib/exsom/xsd/model.ex
@@ -1,7 +1,20 @@
 defmodule Exsom.XSD.Model do
-  # For the cases where a manual schema needs to be supplied
-  # (ex. cannot parse)
-  def from_map(model), do: :erlsom.add_xsd_model(model)
+  @moduledoc ~S"""
+  Model utilities.
 
+  For the cases where a manual schema needs to be supplied
+  (ex. cannot parse).
+  """
+
+  @doc ~S"""
+  Adds a simplified version of the W3C XMLSchema to an existing model.
+  """
+  @spec add_xml_schema(model :: Exsom.model) :: Exsom.model
+  def add_xml_schema(model), do: :erlsom.add_xsd_model(model)
+
+  @doc ~S"""
+  Merges two models.
+  """
+  @spec merge(model_1 :: Exsom.model, model_2 :: Exsom.model) :: Exsom.model
   def merge(model_1, model_2), do: :erlsom.add_model(model_1, model_2)
 end
